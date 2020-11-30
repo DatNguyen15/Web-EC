370de4744b4d3140aa1c306994930b9a03c3a93f
DatNguyen15 <55264571+DatNguyen15@users.noreply.github.com>
2020-12-01 00:34:34 +0700

    Create Home Page

--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,27 +1,93 @@
-import React from "react";
-import {Link, withRouter} from 'react-router-dom';
-
-const isActive = (history,path) =>{
-    if(history.location.pathname === path){
-        return {color: "#ff9900"};
-    }else{
-        return {color:"#ffffff"};
-    }
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { isAuthenticated, signout } from "../auth/index";
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
 };
 
-const Menu = ({history}) =>(
-    <div className="" style={{background:'#333'}}>
-        <ul className="nav nav-tabs container ">
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history,'/')} to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history,'/signin')} to="/signin">Signin</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history,'/signup')} to="/signup">Signup</Link>
-            </li>
-        </ul>
-    </div>
+const Menu = ({ history }) => (
+  <div className="" style={{ background: "#333" }}>
+    <ul className="nav container ">
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, "/")} to="/">
+          Home
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/shop")}
+          to="/shop"
+        >
+          Shop
+        </Link>
+      </li>
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/user/dashboard")}
+            to="/user/dashboard"
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/admin/dashboard")}
+            to="/admin/dashboard"
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+
+      {!isAuthenticated() && (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signin")}
+              to="/signin"
+            >
+              Signin
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signup")}
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </li>
+        </Fragment>
+      )}
+      {isAuthenticated() && (
+        <li className="nav-item">
+          <span
+            className="nav-link"
+            style={{ cursor: "pointer", color: "#ffffff" }}
+            onClick={() =>
+              signout(() => {
+                history.push("/");
+              })
+            }
+          >
+            Signout
+          </span>
+        </li>
+      )}
+    </ul>
+  </div>
 );
-export default withRouter(Menu)
+export default withRouter(Menu);
