d4a88457a5a01901614a54b019de30e41b83b8f2
DatNguyen15 <55264571+DatNguyen15@users.noreply.github.com>
2020-12-01 21:42:28 +0700

    Fontend shop

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import "../dist/css/reset.css";
 import "../dist/css/cardproduct.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 const Card = ({ product }) => {
   return (
     <div className="col-sm-2 col-md-4 mb-3">
@@ -15,7 +16,9 @@ const Card = ({ product }) => {
           </div>
           <div className="desc"></div>
           <p className="pt-3">Description: {product.description}</p>
-          <p>Price: {product.price}$</p>
+          <p>
+            Price: {product.price}$<i className="fas fa-star"></i>
+          </p>
           <Link to="/">
             <div class="button-card">
               <button className="btns1">View Product</button>
--- ecommerce-front/src/core/Checkbox.js
+++ ecommerce-front/src/core/Checkbox.js
@@ -23,10 +23,10 @@ const Checkbox = ({ categories, handleFilters }) => {
         onChange={handleToggle(c._id)}
         value={checked.indexOf(c._id === -1)}
         type="checkbox"
-        id={c}
+        id={i}
         className="form-check-input mr-2 ml-4"
       />
-      <label className="form-check-label mr-3 ml-5" for={c}>
+      <label className="form-check-label mr-3 ml-5" htmlFor={i}>
         {c.name}
       </label>
     </div>
